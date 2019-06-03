5e9eb1094d2bbcacc3b06d339a6e25e882de25fd
Aaron Craelius <aaron@regen.network>
2019-06-03 16:03:08 -0400

    Fix bug in upgrade module impl

--- x/upgrade/module.go
+++ x/upgrade/module.go
@@ -5,7 +5,6 @@ import (
 
 	"github.com/cosmos/cosmos-sdk/codec"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/cosmos/cosmos-sdk/x/gov/types"
 	abci "github.com/tendermint/tendermint/abci/types"
 )
 
@@ -26,7 +25,7 @@ var _ sdk.AppModuleBasic = AppModuleBasic{}
 
 // module name
 func (AppModuleBasic) Name() string {
-	return types.ModuleName
+	return ModuleName
 }
 
 // register module codec
@@ -36,13 +35,13 @@ func (AppModuleBasic) RegisterCodec(cdc *codec.Codec) {
 
 // default genesis state
 func (AppModuleBasic) DefaultGenesis() json.RawMessage {
-	return types.ModuleCdc.MustMarshalJSON(DefaultGenesisState())
+	return moduleCdc.MustMarshalJSON(DefaultGenesisState())
 }
 
 // module validate genesis
 func (AppModuleBasic) ValidateGenesis(bz json.RawMessage) error {
 	var data GenesisState
-	err := types.ModuleCdc.UnmarshalJSON(bz, &data)
+	err := moduleCdc.UnmarshalJSON(bz, &data)
 	if err != nil {
 		return err
 	}
@@ -66,7 +65,7 @@ func NewAppModule(keeper Keeper) AppModule {
 
 // module name
 func (AppModule) Name() string {
-	return types.ModuleName
+	return ModuleName
 }
 
 // register invariants
@@ -87,7 +86,7 @@ func (am AppModule) NewQuerierHandler() sdk.Querier { return nil }
 // module init-genesis
 func (am AppModule) InitGenesis(ctx sdk.Context, data json.RawMessage) []abci.ValidatorUpdate {
 	var genesisState GenesisState
-	types.ModuleCdc.MustUnmarshalJSON(data, &genesisState)
+	moduleCdc.MustUnmarshalJSON(data, &genesisState)
 	InitGenesis(ctx, am.keeper, genesisState)
 	return []abci.ValidatorUpdate{}
 }
@@ -95,7 +94,7 @@ func (am AppModule) InitGenesis(ctx sdk.Context, data json.RawMessage) []abci.Va
 // module export genesis
 func (am AppModule) ExportGenesis(ctx sdk.Context) json.RawMessage {
 	gs := ExportGenesis(ctx, am.keeper)
-	return types.ModuleCdc.MustMarshalJSON(gs)
+	return moduleCdc.MustMarshalJSON(gs)
 }
 
 // module begin-block
