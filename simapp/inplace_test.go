55609f4a8f94ff164381c2bc7b3d62ea33330818
Ethan Frey <ethanfrey@users.noreply.github.com>
2019-07-09 19:32:34 +0200

    First attempt to load old db

--- simapp/inplace_test.go
+++ simapp/inplace_test.go
@@ -1,7 +1,10 @@
 package simapp
 
 import (
+	"io"
+	"io/ioutil"
 	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -11,17 +14,83 @@ import (
 )
 
 func TestInplaceMigrationFrom034(t *testing.T) {
-	db := db.NewMemDB()
+	dbName := "application_034"
+	tmpDir, cleanup := CopyTestdata(t, dbName+".db")
+	defer cleanup()
+
+	ldb, err := db.NewGoLevelDB(dbName, tmpDir)
+	require.Nil(t, err)
 	logger := log.NewTMLogger(log.NewSyncWriter(os.Stdout))
-	app := NewSimApp(logger, db, nil, true, 0)
+	app := NewSimApp(logger, ldb, nil, true, 0)
 
 	info := app.Info(abci.RequestInfo{})
 	require.Equal(t, 1000, info.GetLastBlockHeight())
 	require.NotNil(t, info.GetLastBlockAppHash())
 
-	err := app.inplaceMigration("foobar")
+	err = app.inplaceMigration("foobar")
 	require.NoError(t, err)
 	res := app.Commit()
 	require.NotEmpty(t, res.Data)
 	require.NotEqual(t, info.GetLastBlockAppHash(), res.Data)
+}
+
+func CopyTestdata(t *testing.T, subDir string) (string, func()) {
+	sourceDir := "testdata"
+	destDir, err := ioutil.TempDir("", "test-migration")
+	require.Nil(t, err)
+	cleanup := func() { os.RemoveAll(destDir) }
+
+	err = copyFiles(t, sourceDir, destDir, subDir)
+	if err != nil {
+		cleanup()
+		t.Fatalf("Cannot copy config files: %+v", err)
+	}
+	return destDir, cleanup
+}
+
+func copyFiles(t *testing.T, sourceDir, rootDir, subDir string) error {
+	// make the output dir
+	outDir := filepath.Join(rootDir, subDir)
+	err := os.Mkdir(outDir, 0755)
+	if err != nil {
+		return err
+	}
+
+	// copy everything over from testdata
+	inDir := filepath.Join(sourceDir, subDir)
+	files, err := ioutil.ReadDir(inDir)
+	if err != nil {
+		return err
+	}
+	for _, f := range files {
+		if f.IsDir() {
+			continue
+		}
+		input := filepath.Join(inDir, f.Name())
+		output := filepath.Join(outDir, f.Name())
+		// t.Logf("Copying %s to %s", input, output)
+		err = fileCopy(input, output, f.Mode())
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func fileCopy(input, output string, mode os.FileMode) error {
+	from, err := os.Open(input)
+	if err != nil {
+		return err
+	}
+	defer from.Close()
+
+	to, err := os.OpenFile(output, os.O_WRONLY|os.O_CREATE, mode)
+	if err != nil {
+		return err
+	}
+	defer to.Close()
+
+	_, err = io.Copy(to, from)
+	return err
 }
