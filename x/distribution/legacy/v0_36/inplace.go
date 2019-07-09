c90425b6a7d1d2e5d621f523e6b29bb6fcf278cb
Ethan Frey <ethanfrey@users.noreply.github.com>
2019-07-09 18:26:18 +0200

    Set up placeholders for inplace migrations

---
+++ x/distribution/legacy/v0_36/inplace.go
@@ -0,0 +1,10 @@
+package v0_36
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+// InplaceMigration will upgrade a v0.34 database to a v0.36 database
+func InplaceMigrationFromv034(ctx sdk.Context, distKey sdk.StoreKey) error {
+	return nil
+}
