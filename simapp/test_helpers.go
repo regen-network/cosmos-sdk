584c7ef2138533c0fe628bac22f56dca5b0f0caf
Ethan Frey <ethanfrey@users.noreply.github.com>
2019-12-12 16:10:20 +0100

    Start pulling together function setup

--- simapp/test_helpers.go
+++ simapp/test_helpers.go
@@ -25,23 +25,28 @@ func Setup(isCheckTx bool) *SimApp {
 	db := dbm.NewMemDB()
 	app := NewSimApp(log.NewNopLogger(), db, nil, true, []int64{}, 0)
 	if !isCheckTx {
-		// init chain must be called to stop deliverState from being nil
-		genesisState := NewDefaultGenesisState()
-		stateBytes, err := codec.MarshalJSONIndent(app.cdc, genesisState)
-		if err != nil {
-			panic(err)
-		}
+		SetupDeliverTx(app)
+	}
+	return app
+}
 
-		// Initialize the chain
-		app.InitChain(
-			abci.RequestInitChain{
-				Validators:    []abci.ValidatorUpdate{},
-				AppStateBytes: stateBytes,
-			},
-		)
+// SetupDeliverTx will do general initialization. Can be used even if you want to call
+// NewSimApp with other args than Setup defaults
+func SetupDeliverTx(app *SimApp) {
+	// init chain must be called to stop deliverState from being nil
+	genesisState := NewDefaultGenesisState()
+	stateBytes, err := codec.MarshalJSONIndent(app.cdc, genesisState)
+	if err != nil {
+		panic(err)
 	}
 
-	return app
+	// Initialize the chain
+	app.InitChain(
+		abci.RequestInitChain{
+			Validators:    []abci.ValidatorUpdate{},
+			AppStateBytes: stateBytes,
+		},
+	)
 }
 
 // SetupWithGenesisAccounts initializes a new SimApp with the passed in
