506e37d2e0c954e409a0e8e9517a3f8747b87b34
Sahith Reddy Narahari <sahith@vitwit.com>
2019-12-20 17:25:29 +0530

    Added load upgrade file

--- server/config/upgrade_info.go
+++ server/config/upgrade_info.go
@@ -1,5 +1,31 @@
 package config
 
+import (
+	"encoding/json"
+	"io/ioutil"
+	"os"
+
+	"github.com/spf13/viper"
+	"github.com/tendermint/tendermint/libs/cli"
+
+	store "github.com/cosmos/cosmos-sdk/store/types"
+)
+
 func GenerateOrLoadUpgradeInfoFile() error {
+	home := viper.GetString(cli.HomeFlag)
+	upgradeFilePath := home + "upgrade-info.json"
+	if _, err := os.Stat(upgradeFilePath); err == nil {
+		return nil
+	} else if os.IsNotExist(err) {
+		var upgradeFile store.UpgradeFile
+		upgradeFile.Height = 0
+
+		info, err := json.Marshal(upgradeFile)
+		if err != nil {
+			return err
+		}
+		err = ioutil.WriteFile(upgradeFilePath, info, 0644)
+	}
+
 	return nil
 }
